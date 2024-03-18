325a951f9521908e1d598fdaea7f77cfd4b7bb17
cleberson <clebersonweng@gmail.com>
2024-03-18 09:49:44 -0300

    Created card jobs and list the jobs, show the job description

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\MyJobController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('', fn() => to_route('jobs.index'));
+Route::resource('jobs',MyJobController::class);
